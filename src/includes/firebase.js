5ceb11de107cd85ecaa8563527f1b6911bd21587
Morgan J <40442819+MorganEJLA@users.noreply.github.com>
2023-04-10 17:05:42 -0400

    initial commit

---
+++ src/includes/firebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBG0F1K6JGjCVIs8GeZU6B1Y7uJ2pFArkk',
+  authDomain: 'music-609bb.firebaseapp.com',
+  projectId: 'music-609bb',
+  storageBucket: 'music-609bb.appspot.com',
+  messagingSenderId: '444290921956',
+  appId: '1:444290921956:web:cd619f44c4c8b2f5a683f1'
+}
+
+firebase.initializeApp(firebaseConfig)
+
+const auth = firebase.auth()
+const db = firebase.firestore()
+
+const usersCollection = db.collection('users')
+
+export { auth, db, usersCollection }
