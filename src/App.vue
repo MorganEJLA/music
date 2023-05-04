0d1c94787ddf6619946984e233417cfade4ce2f9
Morgan J <40442819+MorganEJLA@users.noreply.github.com>
2023-05-04 16:54:10 -0400

    worked on route guards wrapped template in main tg

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <app-header></app-header>
+  <app-header />
+  <router-view v-slot="{ Component }">
+    <transition name="fade" mode="out-in">
+      <component :is="Component"></component>
+    </transition>
+  </router-view>
 
-  <router-view></router-view>
-  <app-player></app-player>
-  <app-auth></app-auth>
+  <app-player />
+  <app-auth />
 </template>
 
 <script>
@@ -31,3 +35,17 @@ export default {
   }
 }
 </script>
+<style>
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 0.2s linear;
+}
+
+.fade-leave-to {
+  transition: all 0.2s linear;
+  opacity: 0;
+}
+</style>
