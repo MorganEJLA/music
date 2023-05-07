e84ab9a219b9a75bd55496a064a0eae59c89c420
Morgan J <40442819+MorganEJLA@users.noreply.github.com>
2023-05-06 22:54:11 -0400

    updates

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import VeeValidatePlugin from './includes/validation'
 import './includes/firebase'
 import { auth } from './includes/firebase'
 import Icon from './directives/icon'
+import i18n from './includes/i18n'
+
 import './assets/base.css'
 import './assets/main.css'
 
@@ -19,6 +21,7 @@ auth.onAuthStateChanged(() => {
     app.use(createPinia())
     app.use(router)
     app.use(VeeValidatePlugin)
+    app.use(i18n)
     app.directive('icon', Icon)
 
     app.mount('#app')
